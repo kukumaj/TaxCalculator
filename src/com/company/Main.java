531a69decbd99834c2c6b98b9c8d1e9b2e14669d
ryszard <japan26@interia.pl>
2020-07-06 03:46:36 +0200

    tax calc

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TaxCalculator calculator = getCalculator();
+        calculator.calculateTax();
+    }
+    public static TaxCalculator getCalculator(){
+        return new TaxCalculator2020();
+    }
+}
---
+++ src/com/company/TaxCalculator2020.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class TaxCalculator2020 implements TaxCalculator {
+
+    @Override
+    public float calculateTax() {
+        return 1;
+    }
+    public float calculatInsurance(){
+        return 0;
+    }
+}
